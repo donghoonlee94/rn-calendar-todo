c08d0a5ee065c2ad02e317b0cd6ab4562caef316
이동훈 <94donghoon.lee@gmail.com>
2023-05-04 16:01:20 +0900

    add: calendar UI

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { runPracticeDayjs } from './src/pratice-dayjs';
 import { getCalendarColumns } from './src/util';
 import dayjs from "dayjs";
@@ -9,15 +9,32 @@ export default function App() {
   const now = dayjs();
   const columns = getCalendarColumns(now);
 
+  const renderItem = ({ item: date }) =>  {
+    const dateText = dayjs(date).get('date');
+    const day = dayjs(date).get('day');
+    const color = day === 0 ? '#e67639' : day === 6 ? '#5872d1' : '#2b2b2b';
+    const isCurrentMonth = dayjs(date).isSame(now, 'month');
+
+    return (
+      <View style={{ width: 30, height: 30, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ color, opacity: isCurrentMonth ? 1 : 0.4 }}>{dateText} </Text>
+      </View>
+    )
+  }
+
   useEffect(() => {
     console.log(columns)
   }, [])
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+        contentContainerStyle={{ flex: 1, alignContent: 'center', justifyContent: 'center' }}
+        data={columns}
+        numColumns={7}
+        renderItem={renderItem}
+      />
+    </SafeAreaView>
   );
 }
 
