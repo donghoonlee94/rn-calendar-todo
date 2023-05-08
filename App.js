499f75b6f004999bf9845b8337a43f58067db570
이동훈 <94donghoon.lee@gmail.com>
2023-05-08 11:57:14 +0900

    add: ListHeaderComponent

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { runPracticeDayjs } from './src/pratice-dayjs';
 import { getCalendarColumns, getDayColor, getDayText } from './src/util';
 import dayjs from "dayjs";
+import Margin from './src/Margin';
+import { SimpleLineIcons } from '@expo/vector-icons'; 
 
 export default function App() {
   const now = dayjs();
@@ -12,24 +14,51 @@ export default function App() {
   const Column = ({ text, color, opacity }) => {
 
     return (
-      <View style={{ width: 30, height: 30, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ width: 35, height: 35, justifyContent: 'center', alignItems: 'center' }}>
         <Text style={{ color, opacity }}>{text} </Text>
       </View>      
     );
   };
 
-  const ListHeaderComponent = () => {
-    <View>
-      { /* YYYY MM DD */ }
-      {[0, 1, 2, 3, 4, 5, 6].map(day => {
-        const dateText = getDayText(day);
-        const color = getDayColor(day);
+  const ArrowButton = ({ iconName, onPress }) => {
+    return (
+      <TouchableOpacity onPress={onPress} style={{ paddingHorizontal: 20, paddingVertical: 15 }}>
+        <SimpleLineIcons name={iconName} size={15} color="#404040" />
+      </TouchableOpacity>
+    )
+  }
 
-        return (
-          <Column text={dateText} color={color} opacity={1} />
-        )
-      })}
-    </View>
+  const ListHeaderComponent = () => {
+    const currentDateText = dayjs(now).format('YYYY.MM.DD');
+
+    return (
+      <View>
+        <Margin height={15} />
+
+        <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+          <ArrowButton iconName="arrow-left" onPress={() => {}} />
+
+          <TouchableOpacity>
+            <Text style={{ fontSize: 20, color: "#404040" }}>{currentDateText}</Text>
+          </TouchableOpacity>
+
+          <ArrowButton iconName="arrow-right" onPress={() => {}} />
+        </View>
+
+        <Margin height={15} />
+
+        <View  style={{ flexDirection: 'row' }}>
+          {[0, 1, 2, 3, 4, 5, 6].map(day => {
+            const dateText = getDayText(day);
+            const color = getDayColor(day);
+
+            return (
+              <Column key={`day-${day}`} text={dateText} color={color} opacity={1} />
+            )
+          })}
+        </View>
+      </View>      
+    )
   };
 
   const renderItem = ({ item: date }) =>  {
@@ -52,6 +81,7 @@ export default function App() {
       <FlatList 
         contentContainerStyle={{ flex: 1, alignContent: 'center', justifyContent: 'center' }}
         data={columns}
+        keyExtractor={(_, index) => `column-${index}`}
         numColumns={7}
         renderItem={renderItem}
         ListHeaderComponent={ListHeaderComponent}
