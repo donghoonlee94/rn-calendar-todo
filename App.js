02d9b3e40eb716e678251ebfa207bf5f82226268
이동훈 <94donghoon.lee@gmail.com>
2023-05-08 15:13:46 +0900

    add: react native modal datetime picker

--- App.js
+++ App.js
@@ -6,11 +6,36 @@ import { getCalendarColumns, getDayColor, getDayText } from './src/util';
 import dayjs from "dayjs";
 import Margin from './src/Margin';
 import { SimpleLineIcons } from '@expo/vector-icons'; 
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 export default function App() {
   const now = dayjs();
   const [selectedDate, setSelectedDate] = useState(now);
   const columns = getCalendarColumns(selectedDate);
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    setSelectedDate(dayjs(date));
+    hideDatePicker();
+  };
+
+  const onPressLeftArrow = () =>{
+    const newSelectedDate = dayjs(selectedDate).subtract(1, 'month');
+    setSelectedDate(newSelectedDate);
+  }
+  const onPressRightArrow = () =>{
+    const newSelectedDate = dayjs(selectedDate).add(1, 'month');
+    setSelectedDate(newSelectedDate);
+  }
 
   const Column = ({ text, color, opacity, disabled, onPress, isSelected }) => {
 
@@ -39,7 +64,7 @@ export default function App() {
         <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
           <ArrowButton iconName="arrow-left" onPress={() => {}} />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={showDatePicker}>
             <Text style={{ fontSize: 20, color: "#404040" }}>{currentDateText}</Text>
           </TouchableOpacity>
 
@@ -97,6 +122,13 @@ export default function App() {
         renderItem={renderItem}
         ListHeaderComponent={ListHeaderComponent}
       />
+      
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />      
     </SafeAreaView>
   );
 }
