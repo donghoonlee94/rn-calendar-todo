31f785a4b224095d3e0f6cba8262e9fbf1e99cfa
이동훈 <94donghoon.lee@gmail.com>
2023-05-08 15:52:06 +0900

    add: use-todo-list hook logic

--- src/hook/use-todo-list.js
+++ src/hook/use-todo-list.js
@@ -1,7 +1,37 @@
 import { useState } from "react";
 
-export const useTodoList = () => {
+export const useTodoList = (selectedDate) => {
   const [todoList, setTodoList] = useState([]);
+  const [input, setInput] = useState('');
+
+  const addTodo = () => {
+    const len = todoList.length;
+    const lastId = len === 0 ? 0 : todoList[len - 1].id;
+
+    const newTodoList = [
+      ...todoList,
+      {
+        id: ++lastId, content: input, date: selectedDate, isSuccess: false,
+      }
+    ];
+    setTodoList(newTodoList);
+  };
+
+  const removeTodo = todoId => {
+    const newTodoList = todoList.filter(({ id })  => id !== todoId);
+    setTodoList(newTodoList);
+  };
+
+  const toggleTodo = todoId => {
+    const newTodoList = todoList.map((todo) => {
+      if(todo.id !== todoId) return todo;
+      return {
+        ...todo,
+        isSuccess: !todo.isSuccess
+      }
+    });
+    setTodoList(newTodoList);
+  };
 
   return {};
 };
