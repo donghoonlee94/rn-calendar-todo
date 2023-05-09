c5881ad7382d51077c452cf05f81356eb11eab9d
이동훈 <94donghoon.lee@gmail.com>
2023-05-09 14:01:18 +0900

    add: react-native-iphone-x-helper

--- App.js
+++ App.js
@@ -99,6 +99,11 @@ export default function App() {
     <View style={styles.container}>
       <Image
         source={{ uri: "https://img.freepik.com/free-photo/white-crumpled-paper-texture-for-background_1373-159.jpg?w=1060&t=st=1667524235~exp=1667524835~hmac=8a3d988d6c33a32017e280768e1aa4037b1ec8078c98fe21f0ea2ef361aebf2c", }}
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+        }}
       />
       <FlatList 
         contentContainerStyle={{ flex: 1, alignContent: 'center', justifyContent: 'center' }}
